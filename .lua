d988d429a1e526c99de95a2d1c1aec2349222331
Johnuiy <122029906+Johnuiy@users.noreply.github.com>
2023-01-05 22:42:27 +0900

    Create .lua

---
+++ .lua
@@ -0,0 +1,247 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/Ag4qAQ5p"))()
+
+local Window = Library.CreateLib("Reayr|Eng", "DarkTheme")
+
+local Tab = Window:NewTab("Scripts!")
+
+local Section = Tab:NewSection("Normal scripts")
+
+Section:NewButton("Fly v3 Description ->", "DO NOT CLICK SO MANY TIMES", function()
+
+loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("Hub For Chaos ", "Buttoninfo?", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aidanlolol/MainScript/main/ChaosScriptVersionTwoPointOne.lua"))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("oldest script chaos", "?", function()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/1111-ssss/Chaos_Script/main/Chaos_script.txt'))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("Fly V2", "? ", function()
+
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("hoho hub ", "?", function()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("Infinite yield", "Infinite Yield, we know it already bro", function()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+    print("Tau#2879") 
+
+end)
+
+Section:NewButton("Doors", "support fluxus&hydrogen", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Doors"))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("Domainx", "Domainx", function()
+
+loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
+
+    print("Tau#2879")
+
+end)
+
+local Tab = Window:NewTab("Functions")
+
+local Section = Tab:NewSection("omG daddy🥵")
+
+Section:NewSlider("speed", "aHh dAddy not so FAst", 500, 0, function(s)
+
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+
+end)
+
+Section:NewTextBox("speed v2", "TextboxInfo", function(txt)
+
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = txt
+
+	print(txt)end)
+
+Section:NewSlider("jumppoweR🥵", "", 500, 0, function(s) 
+
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+
+end)
+
+Section:NewTextBox("jumppower v2", "?", function(txt)
+
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = txt
+
+	print(txt)
+
+end)
+
+Section:NewSlider("health", "daddy kills me", 100, 0, function(s) 
+
+    game.Players.LocalPlayer.Character.Humanoid.Health = s
+
+end)
+
+local Tab = Window:NewTab("Credits")
+
+local Section = Tab:NewSection("Yt:MastArceusMx or MastArceusEng")
+
+Section:NewKeybind("Closing Keybind", ".", Enum.KeyCode.LeftControl, function()
+
+	Library:ToggleUI()
+
+end)
+
+local Tab = Window:NewTab("Updates!.     +5!")
+
+local Section = Tab:NewSection("0 Hub opened")
+
+local Section = Tab:NewSection("1. Added new buttons!")
+
+local Section = Tab:NewSection("2.New hub yet out! hub now moveable")
+
+local Section = Tab:NewSection("3.New themes")
+
+local Section = Tab:NewSection("4.New TAB Functions")
+
+local Section = Tab:NewSection("5.New Buttons on (More scripts) and (FE)")
+
+local Tab = Window:NewTab("FE")
+
+local Section = Tab:NewSection("Fe")
+
+Section:NewButton("Creeper", "I'm Patrick", function()
+
+loadstring(game:HttpGet(('https://pastefy.ga/Kqi7ZcbX/raw'),true))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("A hub with fe scripts","Credits DVRKNVSS", function()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Dvrknvss/UniversalFEScriptHub/main/Script'))()
+
+print("Tau#2879")
+
+end)
+
+local Tab = Window:NewTab("More Scripts")
+
+local Section = Tab:NewSection("Yeeeeees dad")
+
+Section:NewButton("Adopt me! Key system", "Asowess", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/Diamond/main/AdoptMe"))()
+
+    print("Tau#2879")
+
+end)
+
+Section:NewButton("Arsenal!","", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Arsenal"))()
+
+print("Tau#2879")
+
+end)
+
+Section:NewButton("Evade","", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Darkrai-X/main/Games/Evade"))()
+
+print("Tau#2879")
+
+end)
+
+Section:NewButton("Storng man!","", function()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/hngamingroblox/scripts/main/strongman%20simulator'))()
+
+print("Tau#2879")
+
+end)
+
+Section:NewButton("Break in",".", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua"))()
+
+print("Tau#2879")
+
+end)
+
+Section:NewButton("break in!",".", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nikita365/Break-In-Story-/main/Break%20In%20Story%20Hub"))()
+
+print("Tau#2879")
+
+end)
+
+Section:NewButton("Mini hub","?", function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MiniNoobie/MINI-HUB-V2/main/FINALLY%20UPDATED%20MINI%20HUB",true))()
+
+print("Tau#2879")
+
+end)
+
+local Tab = Window:NewTab("more.")
+
+local Section = Tab:NewSection("i cant make more tabz")
+
+local Section = Tab:NewSection("i will make new")
+
+local Tab = Window:NewTab("Support!")
+
+local Section = Tab:NewSection("Tau#2879")
+
+local Section = Tab:NewSection("Say on my dm's ~Server~")
+
+game.StarterGui:SetCore("SendNotification", { 
+
+Title = "Reayr👍🏼"; 
+
+Text = "E"; -- what the text says (ofc) 
+
+Duration = 30; 
+
+}) 
+
+wait(1) 
+
+game.StarterGui:SetCore("SendNotification", { 
+
+Title = "E"; 
+
+Text = "MastArceusEng"; 
+
+Duration = 30; 
+
+})
+
+  
